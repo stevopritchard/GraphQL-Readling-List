779decd6348e8db87dbd68d20a2f1bc1a7e834dc
stevopritchard <stephen.a.pritchard@gmail.com>
2024-05-31 14:28:58 +0100

    Finishing touches to styles

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="main">
+      <div id="main">
         <h1>Stephen's Reading List</h1>
         <BookList />
         <AddBook />
